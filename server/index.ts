5ac24bb71955ed5386f550b3a5a51b41693f8d97
wu1du2 <wu1du2@gmail.com>
2026-01-21 20:57:53 +0800

    polish scripts for building on Render

--- server/index.ts
+++ server/index.ts
@@ -321,4 +321,5 @@ function getPublicState() {
   };
 }
 
-httpServer.listen(3000, () => console.log('Server running on 3000'));
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => console.log(`Server running on ${PORT}`));
